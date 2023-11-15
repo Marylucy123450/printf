4f5138a7bb52dddee56f9f02d059c157324cc8be
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-11-15 01:07:24 -0800

    Tired.com

--- _printf.c
+++ _printf.c
@@ -1,22 +1,45 @@
 #include "main.h"
-
 /**
- *_printf - A personalized version of printf for printing formatted output.
- *@format: The string with format instructions.
- *@...: Additional arguments for formatting and printing.
- *
- * Return: Count of printed characters (excluding null byte)
+ * _printf -  function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", print_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEXDEC},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
+
 	va_list args;
-	int count = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
-	count = parse_format(format, args);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
 	va_end(args);
-
-	return (count);
+	return (len);
 }
-
-
--- main.h
+++ main.h
@@ -2,9 +2,29 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
 #include <unistd.h>
 
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
+int _putchar(char c);
+int _strlen(char *s);
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
